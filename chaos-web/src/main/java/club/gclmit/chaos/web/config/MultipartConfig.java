1bf175da472c31f162839d314a12db736c30acbf
gclm <1719982754@qq.com>
2020-03-24 17:35:48 +0800

    feat(chaos-web 发布 1.1.0):
    1. 增加基于 commons-fileupload的文件上传功能

---
+++ chaos-web/src/main/java/club/gclmit/chaos/web/config/MultipartConfig.java
@@ -0,0 +1,33 @@
+package club.gclmit.chaos.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * <p>
+ *  MultipartResolver 配置
+ * </p>
+ *
+ * @author: gclm
+ * @date: 2019/12/18 6:24 下午
+ * @version: V1.0
+ * @since 1.8
+ */
+@Configuration
+public class MultipartConfig  implements WebMvcConfigurer {
+
+    @Bean(name = "multipartResolver")
+    public MultipartResolver multipartResolver(){
+        CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+        resolver.setDefaultEncoding("UTF-8");
+        //resolveLazily属性启用是为了推迟文件解析，以在在UploadAction中捕获文件大小异常
+        resolver.setResolveLazily(true);
+        resolver.setMaxInMemorySize(40960);
+        //上传文件大小 50M 50*1024*1024
+        resolver.setMaxUploadSize(50*1024*1024);
+        return resolver;
+    }
+}
