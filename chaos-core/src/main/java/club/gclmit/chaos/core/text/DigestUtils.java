1806e27d774b0d37cda67d0c049d8a719c4bee2b
gclm <1719982754@qq.com>
2020-05-26 17:23:31 +0800

    fix(chaos-core java doc 日志优化):

--- chaos-core/src/main/java/club/gclmit/chaos/core/text/DigestUtils.java
+++ chaos-core/src/main/java/club/gclmit/chaos/core/text/DigestUtils.java
@@ -23,9 +23,9 @@ public class DigestUtils extends org.springframework.util.DigestUtils {
     /**
      * 默认为 UTF-8的编码格式加密
      *
-     * @author gclm
-     * @param str  内容
+     * @param str 内容
      * @return java.lang.String
+     * @author gclm
      */
     public static String md5(String str) {
         Assert.isFalse(StringUtils.isEmpty(str), "str 不能为空");
@@ -35,9 +35,9 @@ public class DigestUtils extends org.springframework.util.DigestUtils {
     /**
      * 自定义编码格式加密
      *
-     * @author gclm
      * @param str     内容
      * @param charset 编码格式
+     * @author gclm
      * @return: java.lang.String
      */
     public static String md5(String str, String charset) {
@@ -52,9 +52,9 @@ public class DigestUtils extends org.springframework.util.DigestUtils {
     /**
      * md5加密（byte[] To String）
      *
-     * @author gclm
      * @param bytes 内容
      * @return java.lang.String
+     * @author gclm
      */
     public static String md5(byte[] bytes) {
         Assert.isFalse(ArrayUtils.isEmpty(bytes), "bytes 不能为空");
@@ -64,27 +64,34 @@ public class DigestUtils extends org.springframework.util.DigestUtils {
     /**
      * md5加密（File To String）
      *
-     * @author gclm
      * @param file File
      * @return java.lang.String
      * @throws IOException if an I/O error occurs
+     * @author gclm
      */
-    public static String md5(File file) throws IOException {
+    public static String md5(File file) {
         Assert.isFalse(FileUtils.isEmpty(file), "文件不能为空");
-        InputStream in = new FileInputStream(file);
-        return md5(in);
+        try (InputStream in = new FileInputStream(file)) {
+            return md5(in);
+        } catch (IOException e) {
+            throw new ChaosCoreException("md5加密（File To String）失败", e);
+        }
     }
 
     /**
      * md5加密（InputStream To String）
      *
-     * @author gclm
      * @param in InputStream
      * @return java.lang.String
      * @throws IOException if an I/O error occurs
+     * @author gclm
      */
-    public static String md5(InputStream in) throws IOException {
+    public static String md5(InputStream in) {
         Assert.isFalse(IOUtils.isEmpty(in), "输入流 in 不能为空");
-        return md5DigestAsHex(in);
+        try {
+            return md5DigestAsHex(in);
+        } catch (IOException e) {
+            throw new ChaosCoreException("md5加密（InputStream To String）失败", e);
+        }
     }
 }
