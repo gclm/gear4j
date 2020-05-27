9581758d52c73deec6252b2315e374f2b1ae0b83
gclm <1719982754@qq.com>
2020-05-27 19:26:24 +0800

    feat(优化日志服务枚举):

--- chaos-core/src/main/java/club/gclmit/chaos/core/lang/LoggerServer.java
+++ chaos-core/src/main/java/club/gclmit/chaos/core/lang/LoggerServer.java
@@ -9,65 +9,93 @@ package club.gclmit.chaos.core.lang;
  */
 public enum LoggerServer {
 
-    /**
-     * 组件日志
-     */
-    // ~ 框架日志
-    // ===================================================================================================
-
-    /**
-     *   Spring 全家桶
-     */
-    SPRING_BOOT("Spring Boot"),
-    SPRING_SECURITY("Spring Security"),
-    SPRING_JPA("Spring Data JPA"),
-    SPRING_SESSION("Spring Session"),
-    SPRING_SOCIAL("Spring Social"),
-    SPRING_SHELL("Spring Shell"),
-
-    // ~ 其他组件
-    // ===================================================================================================
-
-    MYBATIS("Mybatis"),
-    DBUTILS("DBUtils"),
-    DRUID("Druid"),
-
-    JACKJSON("JackJson"),
-    FASTJSON("FastJSON"),
-
-    SWAGGER("Swagger"),
-
-    WEBSOCKET("WebSocket"),
-    OKHTTP("OkHttp"),
-    HTTPCLIENT("Httpclient"),
-
     // ~ Chaos 组件
     // ===================================================================================================
 
+    /**
+     * Chaos
+     */
     CHAOS("Chaos"),
-    CHAOS_SECURITY("Chaos Security"),
-    GPI("GPI"),
-    CATKIN("Catkin"),
+
+    // ~ MVC 架构 Controller
+    // ===================================================================================================
+
+    /**
+     * Controller
+     */
+    CONTROLLER("Controller"),
+
+    /**
+     * Interceptor
+     */
+    INTERCEPTOR("Interceptor"),
+
+    /**
+     * Filter
+     */
+    FILTER("Filter"),
+
+    /**
+     * Aspect
+     */
+    ASPECT("Aspect"),
+
+    /**
+     * config
+     */
+    CONFIG("config"),
+
+    // ~ MVC 架构 Model
+    // ===================================================================================================
+
+    /**
+     * pojo
+     */
+    POJO("pojo"),
+
+    /**
+     * entity
+     */
+    ENTITY("entity"),
+
+    /**
+     * DTO
+     */
+    DTO("DTO"),
+
+    /**
+     * VO
+     */
+    VO("VO"),
+
+    /**
+     * Param
+     */
+    PARAM("Param"),
+
+    /**
+     * Query
+     */
+    QUERY("Query"),
+
 
     // ~ MVC 架构
     // ===================================================================================================
 
-    CONTROLLER("Controller"),
+    /**
+     * Service
+     */
     SERVICE("Service"),
-    MAPPER("Mapper"),
-    UTILS("Utils"),
-    HELPER("Helper"),
-    INTERCEPTOR("Interceptor"),
-    FILTER("Filter"),
-    ASPECT("Aspect"),
 
-    // model
-    POJO("pojo"),
-    ENTITY("entity"),
-    DTO("DTO"),
-    VO("VO"),
-    PARAM("Param"),
-    QUERY("Query");
+    /**
+     * Mapper
+     */
+    MAPPER("Mapper"),
+
+    /**
+     * Util
+     */
+    UTIL("Util");
 
     private String key;
 
