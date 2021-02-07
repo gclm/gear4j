d5f95cc644c8b152cbaf3a872bf6be4571146fe7
gclm <1719982754@qq.com>
2021-02-07 10:27:03 +0800

    fix(更新工具类):

---
+++ chaos-core/src/main/java/club/gclmit/chaos/core/util/HexUtils.java
@@ -0,0 +1,13 @@
+package club.gclmit.chaos.core.util;
+
+import cn.hutool.core.util.HexUtil;
+
+/**
+ * hex 工具，编解码全用 byte
+ *
+ * @author gclm
+ * @since 2/7/2021 10:14 AM
+ * @since 1.8
+ */
+public class HexUtils extends HexUtil {
+}
