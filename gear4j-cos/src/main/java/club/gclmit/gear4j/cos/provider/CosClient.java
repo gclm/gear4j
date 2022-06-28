be3b241155184857640c201b585c9a8c81255fcb
gclm <gclmit@163.com>
2022-06-28 15:49:18 +0800

    🎨: gear4j-storage调整为cos模块

---
+++ gear4j-cos/src/main/java/club/gclmit/gear4j/cos/provider/CosClient.java
@@ -0,0 +1,73 @@
+package club.gclmit.gear4j.cos.provider;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import club.gclmit.gear4j.cos.model.FileInfo;
+
+/**
+ * 使用抽象工厂模式
+ *
+ * @author <a href="https://blog.gclmit.club">gclm</a>
+ * @since jdk11
+ */
+public interface CosClient {
+
+	/**
+	 * 上传文件
+	 *
+	 * @param file 文件
+	 * @return {@link FileInfo} 文件信息
+	 */
+    FileInfo upload(File file);
+
+	/**
+	 * 上传文件
+	 *
+	 * @param file 文件
+	 * @return {@link FileInfo} 文件信息
+	 */
+    FileInfo upload(MultipartFile file);
+
+	/**
+     * 上传字节数组
+     *
+     * @param data 字节数组
+     * @return {@link FileInfo} 文件信息
+     */
+    FileInfo upload(byte[] data);
+
+	/**
+     * 上传字符串
+     *
+     * @param content 字符串内容
+     * @return {@link FileInfo} 文件信息
+     */
+    FileInfo upload(String content);
+
+	/**
+     * 上传文件使用默认配置
+     *
+     * @param inputStream InputStream
+     * @param fileInfo 文件消息
+     * @return {@link FileInfo}
+     */
+    FileInfo upload(InputStream inputStream, FileInfo fileInfo);
+
+	/**
+	 * 批量删除
+	 *
+	 * @param keys 文件keys
+	 */
+    void batchDelete(List<String> keys);
+
+	/**
+	 * 删除单个
+	 *
+	 * @param key 文件key
+	 */
+    void delete(String key);
+}
---
+++ gear4j-cos/src/main/java/club/gclmit/gear4j/cos/provider/impl/UpyunCosClient.java
@@ -0,0 +1,37 @@
+package club.gclmit.gear4j.cos.provider.impl;
+
+import java.io.InputStream;
+import java.util.List;
+
+import club.gclmit.gear4j.cos.model.CosProvider;
+import club.gclmit.gear4j.cos.model.FileInfo;
+import club.gclmit.gear4j.cos.provider.AbstractCosClient;
+import club.gclmit.gear4j.cos.provider.CosClient;
+
+/**
+ * 又拍云
+ *
+ * @author <a href="https://blog.gclmit.club">gclm</a>
+ * @since jdk11
+ */
+public class UpyunCosClient extends AbstractCosClient implements CosClient {
+
+    public UpyunCosClient(CosProvider cosProvider) {
+        super(cosProvider);
+    }
+
+    @Override
+    public FileInfo upload(InputStream inputStream, FileInfo fileInfo) {
+        return null;
+    }
+
+    @Override
+    public void batchDelete(List<String> keys) {
+
+    }
+
+    @Override
+    public void delete(String key) {
+
+    }
+}
