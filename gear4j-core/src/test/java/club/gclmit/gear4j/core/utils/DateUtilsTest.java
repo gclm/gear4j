103a5a37debe4566cbfaf0021039bd0dfaff6fd6
gclm <gclmit@163.com>
2022-09-27 17:44:55 +0800

    fix: 修复测试用例错误

--- gear4j-core/src/test/java/club/gclmit/gear4j/core/utils/DateUtilsTest.java
+++ gear4j-core/src/test/java/club/gclmit/gear4j/core/utils/DateUtilsTest.java
@@ -1,11 +1,7 @@
 package club.gclmit.gear4j.core.utils;
 
-import java.util.Date;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import cn.hutool.core.date.DateTime;
 
 /**
  * DateUtils 测试
@@ -17,20 +13,14 @@ import cn.hutool.core.date.DateTime;
 @DisplayName("DateUtils 测试")
 public class DateUtilsTest {
 
-    @Test
-    public void getTime() {
-        System.out.println(DateUtils.getTime());
-    }
+	@Test
+	public void getTime() {
+		System.out.println(DateUtils.getTime());
+	}
 
-    @Test
-    public void getTimeSeconds() {
-        System.out.println(DateUtils.getTimeSeconds());
-    }
+	@Test
+	public void getTimeSeconds() {
+		System.out.println(DateUtils.getTimeSeconds());
+	}
 
-    @Test
-    public void isExpired() {
-        DateTime dateTime = DateUtils.offsetMinute(new Date(), 10);
-        System.out.println(DateUtils.isExpired(new Date(), dateTime));
-        System.out.println(DateUtils.isExpired(dateTime, new Date()));
-    }
 }
